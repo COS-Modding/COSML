7ce489d01ef1e3d10e1c0cb2352ed8ca0e7cf437
Dastan21 <loicd21@gmail.com>
2023-11-23 19:51:08 +0100

    move mod loader init into game controller

--- COSML/Patches/GameController.cs
+++ COSML/Patches/GameController.cs
@@ -1,11 +1,34 @@
-﻿using COSML.Modding;
+﻿using COSML.Log;
+using COSML.Modding;
 using MonoMod;
+using System.Threading;
+using UnityEngine;
 
 namespace COSML.Patches
 {
     [MonoModPatch("global::GameController")]
     public class GameController : global::GameController
     {
+        public void Awake()
+        {
+            // Mod Loading
+            if (COSML.LoadState == COSML.ModLoadState.NotStarted)
+            {
+                COSML.LoadState = COSML.ModLoadState.Started;
+
+                // Preload reflection
+                new Thread(ReflectionHelper.PreloadCommonTypes).Start();
+
+                GameObject obj = new();
+                DontDestroyOnLoad(obj);
+                obj.AddComponent<BakingIgnore>().StartCoroutine(COSML.LoadModsInit(obj));
+            }
+            else
+            {
+                Logging.API.Debug($"Already begun mod loading (state {COSML.LoadState})");
+            }
+        }
+
         public void OnApplicationQuit()
         {
             ModHooks.OnApplicationQuit();
--- COSML/Patches/SplashScreen.cs
+++ COSML/Patches/SplashScreen.cs
@@ -1,9 +1,6 @@
 #pragma warning disable 414, 649
 
-using COSML.Log;
 using MonoMod;
-using System.Threading;
-using UnityEngine;
 
 namespace COSML.Patches
 {
@@ -34,23 +31,6 @@ namespace COSML.Patches
             loader.gameObject.SetActive(true);
             loader.SetTrigger("Loop");
             plateformManager.GetSaveManager().PreLoadData();
-
-            // Mod Loading
-            if (COSML.LoadState == COSML.ModLoadState.NotStarted)
-            {
-                COSML.LoadState = COSML.ModLoadState.Started;
-
-                // Preload reflection
-                new Thread(ReflectionHelper.PreloadCommonTypes).Start();
-
-                GameObject obj = new();
-                DontDestroyOnLoad(obj);
-                obj.AddComponent<BakingIgnore>().StartCoroutine(COSML.LoadModsInit(obj));
-            }
-            else
-            {
-                Logging.API.Debug($"MainMenu: Already begun mod loading (state {COSML.LoadState})");
-            }
         }
     }
 }
