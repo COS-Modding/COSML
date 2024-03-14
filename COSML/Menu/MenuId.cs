86bba0eaa1ce002e605a39b3a5f26c1537f4557e
Dastan21 <loicd21@gmail.com>
2024-03-14 17:21:47 +0100

    fix MenuId values missing prefix

--- COSML/Menu/MenuId.cs
+++ COSML/Menu/MenuId.cs
@@ -2,31 +2,31 @@
 {
     public static class MenuId
     {
-        public const string PRESSKEY = "Presskey";
-        public const string ROOT = "Principal";
+        public const string PRESSKEY = "Menu_Presskey";
+        public const string ROOT = "Menu_Principal";
 
-        public const string PLAY = "SelectSave";
-        public const string PLAY_LOAD = "Load";
-        public const string PLAY_DELETE = "Effacer";
+        public const string PLAY = "Menu_SelectSave";
+        public const string PLAY_LOAD = "Menu_Load";
+        public const string PLAY_DELETE = "Menu_Effacer";
 
-        public const string OPTIONS = "Options";
-        public const string OPTIONS_CONTROLS = "OptionsCommandes";
-        public const string OPTIONS_AUDIO = "OptionsAudio";
-        public const string OPTIONS_IMAGE = "OptionsVideo";
+        public const string OPTIONS = "Menu_Options";
+        public const string OPTIONS_CONTROLS = "Menu_OptionsCommandes";
+        public const string OPTIONS_AUDIO = "Menu_OptionsAudio";
+        public const string OPTIONS_IMAGE = "Menu_OptionsVideo";
 
-        public const string PAUSE = "Pause";
-        public const string PAUSE_QUIT = "ConfirmQuitter";
+        public const string PAUSE = "Menu_Pause";
+        public const string PAUSE_QUIT = "Menu_ConfirmQuitter";
 
-        public const string HELP = "Help";
-        public const string HELP_CONTROLS = "HelpControles";
-        public const string HELP_GLYPHS = "HelpGlyphes";
-        public const string HELP_INVENTORY = "HelpInventaire";
-        public const string HELP_INFILTRATION = "HelpInfiltration";
-        public const string HELP_MECHANISMS = "HelpMecanismes";
-        public const string HELP_HINTS = "HelpIndices";
+        public const string HELP = "Menu_Help";
+        public const string HELP_CONTROLS = "Menu_HelpControles";
+        public const string HELP_GLYPHS = "Menu_HelpGlyphes";
+        public const string HELP_INVENTORY = "Menu_HelpInventaire";
+        public const string HELP_INFILTRATION = "Menu_HelpInfiltration";
+        public const string HELP_MECHANISMS = "Menu_HelpMecanismes";
+        public const string HELP_HINTS = "Menu_HelpIndices";
 
-        public const string EULA = "EULA";
+        public const string EULA = "Menu_EULA";
 
-        public const string MODS = "Mods";
+        public const string MODS = "Menu_Mods";
     }
 }
